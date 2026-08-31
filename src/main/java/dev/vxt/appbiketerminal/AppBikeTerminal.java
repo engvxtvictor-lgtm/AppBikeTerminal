c6ba712c31d573395b6240da6e3a0e0b47d9a692
Jose Victor de Moura Rufino <engvxtvictor@gmail.com>
2026-08-30 21:35:14 -0300

    feat: projeto foi criado e foi adicionado boot do sistema

---
+++ src/main/java/dev/vxt/appbiketerminal/AppBikeTerminal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dev.vxt.appbiketerminal;
+
+/**
+ *
+ * @author josev
+ */
+public class AppBikeTerminal {
+
+    public static void main(String[] args) {
+    System.out.println("Salve, man.\nBem vindo ao A.B");        
+    }
+}

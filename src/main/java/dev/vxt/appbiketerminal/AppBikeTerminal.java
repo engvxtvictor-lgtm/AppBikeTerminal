bdb5f8be0ed6ac0e36e98b171c01a5f634315878
Jose Victor de Moura Rufino <engvxtvictor@gmail.com>
2026-09-01 16:32:23 -0300

    feat: calcula valor da corrida

--- src/main/java/dev/vxt/appbiketerminal/AppBikeTerminal.java
+++ src/main/java/dev/vxt/appbiketerminal/AppBikeTerminal.java
@@ -1,5 +1,7 @@
 package dev.vxt.appbiketerminal;
 
+import java.util.Scanner;
+
 public class AppBikeTerminal {
 
     public static void main(String[] args) {
@@ -8,13 +10,33 @@ public class AppBikeTerminal {
     String nomeEstacao = "Centro";
     int quantidadeBikeDisponivel = 12;
     double tarifaPorMinuto = 0.50;
-
-    System.out.println(nomeEstacao);
-    System.out.println(quantidadeBikeDisponivel);
-    System.out.println(tarifaPorMinuto);
     
-    System.out.println("Estacao : " + nomeEstacao);
-    System.out.println("Bicicletas disponiveis : " + quantidadeBikeDisponivel);
-    System.out.println("Tarifa por minuto : " +  tarifaPorMinuto);
+    Scanner entrada = new Scanner(System.in);
+    
+    System.out.println("Salve, informe o numero da matricula, por favor!");
+    
+    int matricula  =  entrada.nextInt();
+    entrada.nextLine();
+    
+    
+   
+    
+    
+    System.out.println("Estacao : " + nomeEstacao); 
+    System.out.println("Bicicletas disponiveis : " + quantidadeBikeDisponivel);    
+    System.out.println("Tarifa por minuto : " +  tarifaPorMinuto);    
+    System.out.println("Matricula informada : " + matricula);
+    
+    System.out.println("Informe a estacao de destino, por favor!: " );
+    String estacaoDestino = entrada.nextLine();
+    System.out.println("Estacao de destino informada: "+ estacaoDestino);
+    
+    System.out.println("Informe o tempo estimado de uso em minutos, por favor!: " ); 
+    int tempoUso = entrada.nextInt();
+    System.out.println("Estimativa de tempo de uso informada: "+ tempoUso);
+    
+    double valorCorrida = tempoUso * tarifaPorMinuto;
+    System.out.printf("Valor total da corrida %.2f", valorCorrida);
+
     }
 }

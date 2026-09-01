588086ee51a4f205199565d04c03bc382ec96b9e
Jose Victor de Moura Rufino <engvxtvictor@gmail.com>
2026-09-01 15:29:52 -0300

    feat: adiciona dados fixos da estacao

--- src/main/java/dev/vxt/appbiketerminal/AppBikeTerminal.java
+++ src/main/java/dev/vxt/appbiketerminal/AppBikeTerminal.java
@@ -1,16 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package dev.vxt.appbiketerminal;
 
-/**
- *
- * @author josev
- */
 public class AppBikeTerminal {
 
     public static void main(String[] args) {
     System.out.println("Salve, man.\nBem vindo ao A.B");        
+    
+    String nomeEstacao = "Centro";
+    int quantidadeBikeDisponivel = 12;
+    double tarifaPorMinuto = 0.50;
+
+    System.out.println(nomeEstacao);
+    System.out.println(quantidadeBikeDisponivel);
+    System.out.println(tarifaPorMinuto);
+    
+    System.out.println("Estacao : " + nomeEstacao);
+    System.out.println("Bicicletas disponiveis : " + quantidadeBikeDisponivel);
+    System.out.println("Tarifa por minuto : " +  tarifaPorMinuto);
     }
 }
